5a9562bd532530efe66cb7a3d78c3513fc93384f
clarer26 <clare@nativetechnolgy.com.au>
2015-10-05 19:35:05 +1100

    nav working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 
-}
+    $('#hamburger').click(function() {
+        console.log('menu clicked!');
+        $('#show-this-on-click').slideToggle('slow');
+    });
+
+});
